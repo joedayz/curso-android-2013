18e40bdbc9517445b1bce36b0f9e9ebf4e8f08e3
Jose Diaz <jose.diaz@joedayz.pe>
2013-08-31 15:57:55 -0500

    Formulario Activity

---
+++ RegistroAlumnosEnClase/src/pe/joedayz/registroalumnosenclase/FormularioActivity.java
@@ -0,0 +1,16 @@
+package pe.joedayz.registroalumnosenclase;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class FormularioActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.formulario);
+	}
+
+
+}
