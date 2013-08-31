6eec66674a682b05683357806469672203934057
Jose Diaz <jose.diaz@joedayz.pe>
2013-08-31 16:58:44 -0500

    Asociando boton guardar o grabar con el crear alumno

--- RegistroAlumnosEnClase/src/pe/joedayz/registroalumnosenclase/FormularioActivity.java
+++ RegistroAlumnosEnClase/src/pe/joedayz/registroalumnosenclase/FormularioActivity.java
@@ -4,6 +4,9 @@ import pe.joedayz.registroalumnosenclase.dao.AlumnoDAO;
 import pe.joedayz.registroalumnosenclase.modelo.Alumno;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class FormularioActivity extends Activity {
 
@@ -18,10 +21,24 @@ public class FormularioActivity extends Activity {
 		helper = new FormularioHelper(this);
 
 
-		Alumno alumno = helper.guardarAlumnoDeFormulario();
+		
 
-		AlumnoDAO dao = new AlumnoDAO(this);
-		dao.grabar(alumno);
+		Button boton = (Button) findViewById(R.id.grabar);
+		boton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Alumno alumno = helper.guardarAlumnoDeFormulario();
+
+				AlumnoDAO dao = new AlumnoDAO(FormularioActivity.this);
+				dao.grabar(alumno);
+				dao.close();
+				
+				
+				finish();
+			}
+		});
 
 
 	}
