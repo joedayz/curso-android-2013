946d61ffa246d7d6dad86925dc3d954737ef2e94
Jose Diaz <jose.diaz@joedayz.pe>
2013-11-02 00:13:03 -0500

    Colocando Log para los estados de un activity

--- HolaMundo/src/pe/joedayz/holamundo/HolaMundo.java
+++ HolaMundo/src/pe/joedayz/holamundo/HolaMundo.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class HolaMundo extends Activity {
 
@@ -25,11 +26,50 @@ public class HolaMundo extends Activity {
 			@Override
 			public void onClick(View clickeado) {
 				Log.i("TAG", "Boton clickeado!");
-				
+				Toast.makeText(HolaMundo.this,"Hola!", Toast.LENGTH_LONG).show();
 			}
 		});
+		
+		Log.i("CICLO DE VIDA", "onCreate");
 	}
 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.i("CICLO DE VIDA", "onDestroy");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.i("CICLO DE VIDA", "onPause");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.i("CICLO DE VIDA", "onRestart");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.i("CICLO DE VIDA", "onResume");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.i("CICLO DE VIDA", "onStart");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.i("CICLO DE VIDA", "onStop");
+	}
+
+	
 	
 
 }
