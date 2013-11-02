c4a368533499e064a3fb0097db6a514c289271e0
Jose Diaz <jose.diaz@joedayz.pe>
2013-11-02 17:36:45 -0500

    Demo mostrando que con 2.2 si se puede llamar servicio externo, con 3 para arriba ya no se puede en el mismo hilo de la vista

--- RegistroAlumnosConectividad/src/pe/joedayz/registroalumnosenclase/ListaAlumnosActivity.java
+++ RegistroAlumnosConectividad/src/pe/joedayz/registroalumnosenclase/ListaAlumnosActivity.java
@@ -2,8 +2,18 @@ package pe.joedayz.registroalumnosenclase;
 
 import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import pe.joedayz.registroalumnosenclase.dao.AlumnoDAO;
 import pe.joedayz.registroalumnosenclase.modelo.Alumno;
+import pe.joedayz.registroalumnosenclase.util.AlumnoConverter;
+import pe.joedayz.registroalumnosenclase.util.WebClient;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -19,70 +29,60 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-
+import android.widget.Toast;
 
 public class ListaAlumnosActivity extends Activity {
 
 	private ListView lista;
 	private Alumno alumno;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listade_alumnos);
-		
-	
+
 		lista = (ListView) findViewById(R.id.lista);
-		
+
 		registerForContextMenu(lista);
-		
+
 		lista.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> adapter, View view, 
-					int position,
-					long id) {
-				Alumno alumnoSeleccionado = 
-						(Alumno) adapter.getItemAtPosition(position);
-				Intent irParaFormulario = new Intent(ListaAlumnosActivity.this, FormularioActivity.class);
-				irParaFormulario.putExtra("alumnoSeleccionado", alumnoSeleccionado);
+			public void onItemClick(AdapterView<?> adapter, View view,
+					int position, long id) {
+				Alumno alumnoSeleccionado = (Alumno) adapter
+						.getItemAtPosition(position);
+				Intent irParaFormulario = new Intent(ListaAlumnosActivity.this,
+						FormularioActivity.class);
+				irParaFormulario.putExtra("alumnoSeleccionado",
+						alumnoSeleccionado);
 				startActivity(irParaFormulario);
 			}
-			
+
 		});
 		lista.setOnItemLongClickListener(new OnItemLongClickListener() {
 
-			
-
 			@Override
-			public boolean onItemLongClick(AdapterView<?>
-					adapter, View view,
+			public boolean onItemLongClick(AdapterView<?> adapter, View view,
 					int posicion, long id) {
 				alumno = (Alumno) adapter.getItemAtPosition(posicion);
-//				Toast.makeText(ListaAlumnosActivity.this,
-//						"Clic largo en " + 
-//				alumno, 
-//				Toast.LENGTH_SHORT).show();
+				// Toast.makeText(ListaAlumnosActivity.this,
+				// "Clic largo en " +
+				// alumno,
+				// Toast.LENGTH_SHORT).show();
 				return false;
 			}
-		
 
-	
-			
-		});		
+		});
 	}
 
-	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
 		cargarLista();
 	}
-
 
 	private void cargarLista() {
 		AlumnoDAO dao = new AlumnoDAO(this);
@@ -90,18 +90,16 @@ public class ListaAlumnosActivity extends Activity {
 		List<Alumno> alumnos = dao.getLista();
 		dao.close();
 
-		//int layout = android.R.layout.simple_list_item_1;
-		//usamos ahora nuestro layout
+		// int layout = android.R.layout.simple_list_item_1;
+		// usamos ahora nuestro layout
 		int layout = R.layout.linea_listade;
-		
-		ListaAlumnosAdapter adapter = new 
-				ListaAlumnosAdapter(alumnos, this);
+
+		ListaAlumnosAdapter adapter = new ListaAlumnosAdapter(alumnos, this);
 
 		LayoutInflater inflater = getLayoutInflater();
-		
+
 		lista.setAdapter(adapter);
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -111,45 +109,58 @@ public class ListaAlumnosActivity extends Activity {
 
 		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		
+
 		int itemSeleccionado = item.getItemId();
 		switch (itemSeleccionado) {
 		case R.id.nuevo:
 			Intent irParaFormulario = new Intent(this, FormularioActivity.class);
 			startActivity(irParaFormulario);
 			break;
+		case R.id.enviar_alumnos:
+			String urlServer = "http://www.caelum.com.br/mobile";
 
+			AlumnoDAO dao = new AlumnoDAO(this);
+			List<Alumno> alumnos = dao.getLista();
+			dao.close();
+
+			
+			String datosJSON = 
+					new AlumnoConverter().toJSON(alumnos);
+			
+			
+			WebClient client = new WebClient(urlServer);
+			
+			String respuestaJSON = client.post(datosJSON);
+			
+			Toast.makeText(this, respuestaJSON,
+					Toast.LENGTH_LONG).show();
+			
+			break;
+			
 		default:
 			break;
 		}
-		
+
 		return super.onOptionsItemSelected(item);
 	}
 
-	
-	
-
-
 	@Override
-	public void onCreateContextMenu(ContextMenu menu, 
-			View v,
+	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
-		
-		
+
 		MenuItem llamar = menu.add("Llamar");
 		llamar.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-			
+
 			@Override
 			public boolean onMenuItemClick(MenuItem item) {
-				
-				Intent irALlamar = 
-						new Intent(Intent.ACTION_CALL);
+
+				Intent irALlamar = new Intent(Intent.ACTION_CALL);
 				Uri llamarA = Uri.parse("tel:" + alumno.getTelefono());
 				irALlamar.setData(llamarA);
-				
+
 				startActivity(irALlamar);
 				return false;
 			}
@@ -157,29 +168,27 @@ public class ListaAlumnosActivity extends Activity {
 		menu.add("Enviar SMS");
 		MenuItem site = menu.add("Navegar a");
 		site.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-			
+
 			@Override
 			public boolean onMenuItemClick(MenuItem item) {
-				Intent irASite =
-						new Intent(Intent.ACTION_VIEW);
-				Uri site = Uri.parse("http://"+ alumno.getSite());
+				Intent irASite = new Intent(Intent.ACTION_VIEW);
+				Uri site = Uri.parse("http://" + alumno.getSite());
 				irASite.setData(site);
-				
+
 				startActivity(irASite);
 				return false;
 			}
 		});
-		
-		
+
 		MenuItem eliminar = menu.add("Eliminar");
-		eliminar.setOnMenuItemClickListener( new OnMenuItemClickListener() {
-			
+		eliminar.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
 			@Override
 			public boolean onMenuItemClick(MenuItem item) {
 				AlumnoDAO dao = new AlumnoDAO(ListaAlumnosActivity.this);
 				dao.eliminar(alumno);
 				dao.close();
-				
+
 				cargarLista();
 				return false;
 			}
@@ -188,8 +197,5 @@ public class ListaAlumnosActivity extends Activity {
 		menu.add("Enviar en el email");
 		super.onCreateContextMenu(menu, v, menuInfo);
 	}
-	
-	
-	
-	
+
 }
---
+++ RegistroAlumnosConectividad/src/pe/joedayz/registroalumnosenclase/util/WebClient.java
@@ -0,0 +1,43 @@
+package pe.joedayz.registroalumnosenclase.util;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class WebClient {
+
+	private String urlServer;
+	
+	public WebClient(String urlServer ){
+		this.urlServer = urlServer;
+	}
+	
+	
+	public String post(String datosEnJSON){
+		try {
+			
+			HttpClient client = new DefaultHttpClient();
+			HttpPost post = new HttpPost(urlServer);
+
+			post.setEntity(new StringEntity(datosEnJSON));
+			post.setHeader("Accept", "application/json");
+			post.setHeader("Content-type", "application/json");
+			
+			HttpResponse response = client.execute(post);
+			HttpEntity respuesta  = response.getEntity();
+			
+			String respuestaEnJSON = 
+					EntityUtils.toString(respuesta);
+			
+			return respuestaEnJSON;
+		} catch (Exception e) {
+
+			throw new RuntimeException(e);
+		}
+
+	}
+}
